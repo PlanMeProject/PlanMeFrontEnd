ea08c2dd1017ee9e7e8b6cf3e3b66fa0362eb8c0
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-19 21:09:20 +0700

    clear some elements

--- frontend/src/views/admin/todoDashboard/index.jsx
+++ frontend/src/views/admin/todoDashboard/index.jsx
@@ -165,29 +165,6 @@ export default function UserReports() {
                     value='Completed'
                 />
             </SimpleGrid>
-
-            {/*<SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px' mb='20px'>*/}
-            {/*    <TotalSpent/>*/}
-            {/*    <WeeklyRevenue/>*/}
-            {/*</SimpleGrid>*/}
-            {/*<SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>*/}
-            {/*    <CheckTable columnsData={columnsDataCheck}*/}
-            {/*                tableData={tableDataCheck}/>*/}
-            {/*    <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>*/}
-            {/*        <DailyTraffic/>*/}
-            {/*        <PieCard/>*/}
-            {/*    </SimpleGrid>*/}
-            {/*</SimpleGrid>*/}
-            {/*<SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>*/}
-            {/*    <ComplexTable*/}
-            {/*        columnsData={columnsDataComplex}*/}
-            {/*        tableData={tableDataComplex}*/}
-            {/*    />*/}
-            {/*    <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>*/}
-            {/*        <Tasks/>*/}
-            {/*        <MiniCalendar h='100%' minW='100%' selectRange={false}/>*/}
-            {/*    </SimpleGrid>*/}
-            {/*</SimpleGrid>*/}
             <FixedPlugin/>
         </Box>
     );
