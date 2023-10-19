ea85ae93648ccec2f9ab123397c8e33bb79261e4
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-19 21:06:54 +0700

    push to taskboard after login

--- frontend/src/views/auth/signIn/auth.js
+++ frontend/src/views/auth/signIn/auth.js
@@ -1,27 +1,31 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {initializeApp} from "firebase/app";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const handleGoogleSignIn = async () => {
+export const handleGoogleSignIn = async (history) => {
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
 
     try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log('Signed in user:', user);
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
+        localStorage.setItem('name', user.displayName);
+        console.log('Signed in user:', user.displayName);
+        // redirect to taskboard
+        history.push('/admin/taskboard');
+        return user;
     } catch (error) {
-      console.error('Error signing in with Google:', error);
+        console.error('Error signing in with Google:', error);
     }
 };
