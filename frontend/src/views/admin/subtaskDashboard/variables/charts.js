cf0885f803ac426eaf708b0180d9cee6280ac853
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 13:50:09 +0700

    added charts.js

---
+++ frontend/src/views/admin/subtaskDashboard/variables/charts.js
@@ -0,0 +1,32 @@
+
+export const pieChartData = [75, 25];
+
+
+export const pieChartOptions = {
+    labels: ["Complete", "In Progress"],
+    colors: ["#4318FF", "#6AD2FF"],
+    chart: {
+        width: "50px",
+    },
+    legend: {
+        show: false,
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    hover: {mode: null},
+    plotOptions: {
+        donut: {
+            expandOnClick: false,
+            donut: {
+                labels: {
+                    show: false,
+                },
+            },
+        },
+    },
+    tooltip: {
+        enabled: true,
+        theme: "dark",
+    },
+};
