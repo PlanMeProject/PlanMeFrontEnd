1b22cd8366a753ad80d40858cd170086ed73332f
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 20:36:08 +0700

    passing task data prop

--- frontend/src/views/admin/subtaskDashboard/components/PieCard.js
+++ frontend/src/views/admin/subtaskDashboard/components/PieCard.js
@@ -32,53 +32,56 @@ export default function Conversion(props) {
                 </Text>
             </Flex>
 
-      <PieChart
-        h='100%'
-        w='100%'
-        chartData={pieChartData}
-        chartOptions={pieChartOptions}
-      />
-      <Card
-        bg={cardColor}
-        flexDirection='row'
-        boxShadow={cardShadow}
-        w='100%'
-        p='15px'
-        px='20px'
-        mt='15px'
-        mx='auto'>
-        <Flex direction='column' py='5px'>
-          <Flex align='center'>
-            <Box h='8px' w='8px' bg='brand.500' borderRadius='50%' me='4px' />
-            <Text
-              fontSize='xs'
-              color='secondaryGray.600'
-              fontWeight='700'
-              mb='5px'>
-              Your files
-            </Text>
-          </Flex>
-          <Text fontSize='lg' color={textColor} fontWeight='700'>
-            63%
-          </Text>
-        </Flex>
-        <VSeparator mx={{ base: "60px", xl: "60px", "2xl": "60px" }} />
-        <Flex direction='column' py='5px' me='10px'>
-          <Flex align='center'>
-            <Box h='8px' w='8px' bg='#6AD2FF' borderRadius='50%' me='4px' />
-            <Text
-              fontSize='xs'
-              color='secondaryGray.600'
-              fontWeight='700'
-              mb='5px'>
-              System
-            </Text>
-          </Flex>
-          <Text fontSize='lg' color={textColor} fontWeight='700'>
-            25%
-          </Text>
-        </Flex>
-      </Card>
-    </Card>
-  );
+            <PieChart
+                h='100%'
+                w='100%'
+                chartData={taskData}
+                chartOptions={pieChartOptions}
+            />
+
+            <Card
+                bg={cardColor}
+                flexDirection='row'
+                boxShadow={cardShadow}
+                w='100%'
+                p='15px'
+                px='20px'
+                mt='15px'
+                mx='auto'>
+                <Flex direction='column' py='5px'>
+                    <Flex align='center'>
+                        <Box h='8px' w='8px' bg='brand.500' borderRadius='50%'
+                             me='4px'/>
+                        <Text
+                            fontSize='xs'
+                            color='secondaryGray.600'
+                            fontWeight='700'
+                            mb='5px'>
+                            Completed
+                        </Text>
+                    </Flex>
+                    <Text fontSize='lg' color={textColor} fontWeight='700'>
+                        {taskData[0]}%
+                    </Text>
+                </Flex>
+                <VSeparator mx={{base: "60px", xl: "60px", "2xl": "60px"}}/>
+                <Flex direction='column' py='5px' me='10px'>
+                    <Flex align='center'>
+                        <Box h='8px' w='8px' bg='#6AD2FF' borderRadius='50%'
+                             me='4px'/>
+                        <Text
+                            fontSize='xs'
+                            color='secondaryGray.600'
+                            fontWeight='700'
+                            mb='5px'>
+                            In Progress
+                        </Text>
+                    </Flex>
+                    <Text fontSize='lg' color={textColor} fontWeight='700'>
+                        {taskData[1]}%
+                    </Text>
+                </Flex>
+            </Card>
+        </Card>
+    );
 }
