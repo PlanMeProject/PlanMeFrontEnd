54eeee5ed4d3568def056d810e20e819e2d0a0e3
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 20:55:10 +0700

    added PieCard in grid

--- frontend/src/views/admin/subtaskDashboard/components/Tasks.js
+++ frontend/src/views/admin/subtaskDashboard/components/Tasks.js
@@ -81,7 +81,7 @@ export default function Conversion(props) {
             // You can set an error state here to show an error message if you want
             return;
         }
-        const newId = subtasks.length===0 ? 1: Math.max(...subtasks.map(task => task.id))+1;
+        const newId = subtasks.length === 0 ? 1 : Math.max(...subtasks.map(task => task.id)) + 1;
         // Add the new subtask
         setSubtasks([...subtasks, {...newSubtask, id: newId}]);
         setIsOpen(false);
@@ -109,115 +109,122 @@ export default function Conversion(props) {
     const boxBg = useColorModeValue("secondaryGray.300", "navy.700");
     const brandColor = useColorModeValue("brand.500", "orange.500");
     return (
-        <Card p='20px' align='center' direction='column' w='100%' {...rest}>
-            <Flex alignItems='center' w='100%' mb='30px'>
-                <IconBox
-                    me='12px'
-                    w='38px'
-                    h='38px'
-                    bg={boxBg}
-                    icon={<Icon as={MdAssignment} color={brandColor} w='24px'
-                                h='24px'/>}
-                />
-                <Text color={textColor} fontSize='lg' fontWeight='700'>
-                    Subtasks
-                </Text>
-                {/* Button to toggle subtask visibility */}
-                <Flex display='inline-flex' ml='auto' alignItems='center'>
-                    <Button mr='10px' onClick={loadData}>
-                        Generate
-                    </Button>
-                    <Icon
-                        ms='auto'
-                        mr='10px'
-                        as={MdAdd}
-                        color='green.300'
-                        w='24px'
-                        h='24px'
-                        cursor='pointer'
-                        onClick={() => setIsOpen(true)}
+        <SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>
+            <Card p='20px' align='center' direction='column'
+                  w='100%' {...rest}>
+                <Flex alignItems='center' w='100%' mb='30px'>
+                    <IconBox
+                        me='12px'
+                        w='38px'
+                        h='38px'
+                        bg={boxBg}
+                        icon={<Icon as={MdAssignment} color={brandColor}
+                                    w='24px'
+                                    h='24px'/>}
                     />
-                </Flex>
-
-            </Flex>
-            {/* Add this Modal JSX */}
-            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-                <ModalOverlay/>
-                <ModalContent backgroundColor={boxBg}>
-                    <ModalHeader>Add a new subtask</ModalHeader>
-                    <ModalCloseButton/>
-                    <ModalBody>
-                        <FormControl isRequired>
-                            <FormLabel>Title</FormLabel>
-                            <Input
-                                placeholder="Subtask title"
-                                value={newSubtask.title}
-                                onChange={(e) => setNewSubtask({
-                                    ...newSubtask,
-                                    title: e.target.value
-                                })}
-                                isInvalid={newSubtask.title === ""}
-                                color={textColor}
-                            />
-                            {newSubtask.title === "" &&
-                                <FormHelperText color="red.500">Title is
-                                    required</FormHelperText>}
-                        </FormControl>
-
-                        <FormControl mt={4}>
-                            <FormLabel>Status</FormLabel>
-                            <Select
-                                placeholder="Select status"
-                                value={newSubtask.status}
-                                onChange={(e) => setNewSubtask({
-                                    ...newSubtask,
-                                    status: e.target.value
-                                })}
-                            >
-                                <option value="Incomplete">Incomplete</option>
-                                <option value="Completed">Completed</option>
-                            </Select>
-                        </FormControl>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme="blue" mr={3}
-                                onClick={handleSubmit}>
-                            Submit
+                    <Text color={textColor} fontSize='lg' fontWeight='700'>
+                        Subtasks
+                    </Text>
+                    {/* Button to toggle subtask visibility */}
+                    <Flex display='inline-flex' ml='auto' alignItems='center'>
+                        <Button mr='10px' onClick={loadData}>
+                            Generate
                         </Button>
-                        <Button
-                            onClick={() => setIsOpen(false)}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+                        <Icon
+                            ms='auto'
+                            mr='10px'
+                            as={MdAdd}
+                            color='green.300'
+                            w='24px'
+                            h='24px'
+                            cursor='pointer'
+                            onClick={() => setIsOpen(true)}
+                        />
+                    </Flex>
 
-            {/* Conditionally render subtasks */}
-            {
-                <Box px='11px'>
-                    {subtasks.map((task, index) => (
-                        <Flex key={index} mb='20px'>
-                            <Checkbox me='16px' colorScheme='brandScheme'
-                                      isChecked={task.status === 'Completed'}
-                                      onChange={(e) => handleCheckboxChange(task.id, e.target.checked ? 'Completed' : 'Incomplete')}
-                            />
-                            <Text fontWeight='bold' color={textColor}
-                                  fontSize='md'
-                                  textAlign='start'>
-                                {task.title}
-                            </Text>
-                            <Icon
-                                ms='auto'
-                                as={MdDelete}
-                                color='secondaryGray.600'
-                                w='24px'
-                                h='24px'
-                                cursor='pointer'
-                                onClick={() => handleDelete(task.id)}
-                            />
-                        </Flex>
-                    ))}
-                </Box>
-            }
-        </Card>
+                </Flex>
+                {/* Add this Modal JSX */}
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    <ModalOverlay/>
+                    <ModalContent backgroundColor={boxBg}>
+                        <ModalHeader>Add a new subtask</ModalHeader>
+                        <ModalCloseButton/>
+                        <ModalBody>
+                            <FormControl isRequired>
+                                <FormLabel>Title</FormLabel>
+                                <Input
+                                    placeholder="Subtask title"
+                                    value={newSubtask.title}
+                                    onChange={(e) => setNewSubtask({
+                                        ...newSubtask,
+                                        title: e.target.value
+                                    })}
+                                    isInvalid={newSubtask.title === ""}
+                                    color={textColor}
+                                />
+                                {newSubtask.title === "" &&
+                                    <FormHelperText color="red.500">Title is
+                                        required</FormHelperText>}
+                            </FormControl>
+
+                            <FormControl mt={4}>
+                                <FormLabel>Status</FormLabel>
+                                <Select
+                                    placeholder="Select status"
+                                    value={newSubtask.status}
+                                    onChange={(e) => setNewSubtask({
+                                        ...newSubtask,
+                                        status: e.target.value
+                                    })}
+                                >
+                                    <option value="Incomplete">Incomplete
+                                    </option>
+                                    <option value="Completed">Completed
+                                    </option>
+                                </Select>
+                            </FormControl>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme="blue" mr={3}
+                                    onClick={handleSubmit}>
+                                Submit
+                            </Button>
+                            <Button
+                                onClick={() => setIsOpen(false)}>Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+
+                {/* Conditionally render subtasks */}
+                {
+                    <Box px='11px'>
+                        {subtasks.map((task, index) => (
+                            <Flex key={index} mb='20px'>
+                                <Checkbox me='16px' colorScheme='brandScheme'
+                                          isChecked={task.status === 'Completed'}
+                                          onChange={(e) => handleCheckboxChange(task.id, e.target.checked ? 'Completed' : 'Incomplete')}
+                                />
+                                <Text fontWeight='bold' color={textColor}
+                                      fontSize='md'
+                                      textAlign='start'>
+                                    {task.title}
+                                </Text>
+                                <Icon
+                                    ms='auto'
+                                    as={MdDelete}
+                                    color='secondaryGray.600'
+                                    w='24px'
+                                    h='24px'
+                                    cursor='pointer'
+                                    onClick={() => handleDelete(task.id)}
+                                />
+                            </Flex>
+                        ))}
+                    </Box>
+                }
+            </Card>
+            <PieCard taskData={taskData}/>
+        </SimpleGrid>
     );
 }
