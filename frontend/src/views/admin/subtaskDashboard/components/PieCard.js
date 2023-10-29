62069b0072a93eca4f221868da24ff51b0d7719c
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 20:35:22 +0700

    add constant

--- frontend/src/views/admin/subtaskDashboard/components/PieCard.js
+++ frontend/src/views/admin/subtaskDashboard/components/PieCard.js
@@ -8,37 +8,29 @@ import { VSeparator } from "components/separator/Separator";
 import React from "react";
 
 export default function Conversion(props) {
-  const { ...rest } = props;
+    const { ...rest } = props;
+    const { taskData } = props;
+    // Chakra Color Mode
+    const textColor = useColorModeValue("secondaryGray.900", "white");
+    const cardColor = useColorModeValue("white", "navy.700");
+    const cardShadow = useColorModeValue(
+        "0px 18px 40px rgba(112, 144, 176, 0.12)",
+        "unset"
+    );
 
-  // Chakra Color Mode
-  const textColor = useColorModeValue("secondaryGray.900", "white");
-  const cardColor = useColorModeValue("white", "navy.700");
-  const cardShadow = useColorModeValue(
-    "0px 18px 40px rgba(112, 144, 176, 0.12)",
-    "unset"
-  );
-  return (
-    <Card p='20px' align='center' direction='column' w='100%' {...rest}>
-      <Flex
-        px={{ base: "0px", "2xl": "10px" }}
-        justifyContent='space-between'
-        alignItems='center'
-        w='100%'
-        mb='8px'>
-        <Text color={textColor} fontSize='md' fontWeight='600' mt='4px'>
-          Your Pie Chart
-        </Text>
-        <Select
-          fontSize='sm'
-          variant='subtle'
-          defaultValue='monthly'
-          width='unset'
-          fontWeight='700'>
-          <option value='daily'>Daily</option>
-          <option value='monthly'>Monthly</option>
-          <option value='yearly'>Yearly</option>
-        </Select>
-      </Flex>
+    return (
+        <Card p='20px' align='center' direction='column' w='100%' {...rest}>
+            <Flex
+                px={{base: "0px", "2xl": "10px"}}
+                justifyContent='space-between'
+                alignItems='center'
+                w='100%'
+                mb='8px'>
+                <Text color={textColor} fontSize='md' fontWeight='600'
+                      mt='4px'>
+                    Pie Chart
+                </Text>
+            </Flex>
 
       <PieChart
         h='100%'
