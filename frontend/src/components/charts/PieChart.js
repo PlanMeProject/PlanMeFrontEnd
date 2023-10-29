ff7b8161828c5724ff3dac19c54ae650940055bb
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 20:42:45 +0700

    format line

--- frontend/src/components/charts/PieChart.js
+++ frontend/src/components/charts/PieChart.js
@@ -2,21 +2,21 @@ import React from "react";
 import ReactApexChart from "react-apexcharts";
 
 class PieChart extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      chartData: [],
-      chartOptions: {},
-    };
-  }
+        this.state = {
+            chartData: [],
+            chartOptions: {},
+        };
+    }
 
-  componentDidMount() {
-    this.setState({
-      chartData: this.props.chartData,
-      chartOptions: this.props.chartOptions,
-    });
-  }
+    componentDidMount() {
+        this.setState({
+            chartData: this.props.chartData,
+            chartOptions: this.props.chartOptions,
+        });
+    }
 
   render() {
     return (
