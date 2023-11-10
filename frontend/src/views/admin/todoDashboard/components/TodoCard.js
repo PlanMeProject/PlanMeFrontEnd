94dee09d7b76c67a42c64ce4fd48258161a8ca66
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-10 11:41:14 +0700

    added confirm delete dialog

--- frontend/src/views/admin/todoDashboard/components/TodoCard.js
+++ frontend/src/views/admin/todoDashboard/components/TodoCard.js
@@ -105,6 +105,34 @@ export default function Default(props) {
                     ))}
                 </div>
             </Flex>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            Delete Task
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure? You can't undo this action
+                            afterwards.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme='red' onClick={confirmDelete}
+                                    ml={3}>
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
         </Card>
     );
 }
