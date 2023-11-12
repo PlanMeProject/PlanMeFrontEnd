0903ee5ec7de3da2c6af89601dcd71b392044c89
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-12 12:55:14 +0700

    added user information scope

--- frontend/src/views/auth/signIn/auth.js
+++ frontend/src/views/auth/signIn/auth.js
@@ -4,7 +4,8 @@ const SCOPES = [
     "https://www.googleapis.com/auth/classroom.courses.readonly",
     "https://www.googleapis.com/auth/classroom.rosters.readonly",
     "https://www.googleapis.com/auth/classroom.course-work.readonly",
-    "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly"
+    "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
+    "https://www.googleapis.com/auth/userinfo.email",
 ];
 
 export const handleGoogleSignIn = () => {
