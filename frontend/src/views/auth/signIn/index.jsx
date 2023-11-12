3da64799b298b997ada55c5585e6363e14fc984f
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-12 09:49:19 +0700

    Added the components back except img

--- frontend/src/views/auth/signIn/index.jsx
+++ frontend/src/views/auth/signIn/index.jsx
@@ -25,25 +25,70 @@ function SignIn() {
 
     return (
         <DefaultAuth image={"image_3d_placeholder"}>
-            {/* ... Rest of your component */}
-            <Button
-                onClick={handleGoogleSignIn}
-                fontSize='sm'
-                me='0px'
-                mb='26px'
-                py='15px'
-                h='50px'
-                borderRadius='16px'
-                bg={googleBg}
-                color={googleText}
-                fontWeight='500'
-                _hover={googleHover}
-                _active={googleActive}
-                _focus={googleActive}
-            >
-                <Icon as={FcGoogle} w='20px' h='20px' me='10px'/>
-                Sign in with Google
-            </Button>
+            <Flex
+                // bg='red.500'
+                maxW={{base: "100%", md: "max-content"}}
+                w='100%'
+                mx={{base: "auto", lg: "0px"}}
+                me='auto'
+                h='100%'
+                alignItems='start'
+                justifyContent='center'
+                mb={{base: "30px", md: "60px"}}
+                px={{base: "25px", md: "0px"}}
+                mt={{base: "40px", md: "14vh"}}
+                flexDirection='column'>
+                <Box me='auto'>
+
+                    <Flex display='inline-flex'>
+                        <Icon mt='4px' ml='5px' mr='5px' w='37px' h='37px'
+                              as={MdTask}
+                              color={iconColor}/>
+                        <Heading color={textColor} fontSize='36px' mb='10px'>
+                            PlanMe
+                        </Heading>
+                    </Flex>
+
+                    <Text
+                        mb='36px'
+                        ms='4px'
+                        color={textColorSecondary}
+                        fontWeight='400'
+                        fontSize='md'>
+                        Please sign in with your google account!
+                    </Text>
+                </Box>
+                <Flex
+                    zIndex='2'
+                    direction='column'
+                    w={{base: "100%", md: "420px"}}
+                    maxW='100%'
+                    background='transparent'
+                    borderRadius='15px'
+                    mx={{base: "auto", lg: "unset"}}
+                    me='auto'
+                    mb={{base: "20px", md: "auto"}}>
+                    <Button
+                        onClick={handleGoogleSignIn}
+                        fontSize='sm'
+                        me='0px'
+                        mb='26px'
+                        py='15px'
+                        h='50px'
+                        borderRadius='16px'
+                        bg={googleBg}
+                        color={googleText}
+                        fontWeight='500'
+                        _hover={googleHover}
+                        _active={googleActive}
+                        _focus={googleActive}
+                    >
+                        <Icon as={FcGoogle} w='20px' h='20px' me='10px'/>
+                        Sign in with Google
+                    </Button>
+                </Flex>
+            </Flex>
+
         </DefaultAuth>
     );
 }
