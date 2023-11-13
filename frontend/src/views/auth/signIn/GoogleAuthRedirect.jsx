89b8e39444b2b6fcc00f2c790deedda0ed373f8f
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-13 21:18:23 +0700

    pass the subject as a url param

--- frontend/src/views/auth/signIn/GoogleAuthRedirect.jsx
+++ frontend/src/views/auth/signIn/GoogleAuthRedirect.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {useHistory, useLocation} from 'react-router-dom';
 
 const GoogleAuthHandler = () => {
-    return <div>PRAJAOOOOO HELP ME</div>
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const code = searchParams.get('code');
+    const history = useHistory();
+    const subjects = ['Math', 'Science', 'English', 'History', 'Art', 'Music', 'PE', 'Computer Science', 'Other'];
+
+    useEffect(() => {
+        console.log('Received code:', code);
+        // Redirect with subject as a URL parameter
+        history.push(`/admin/task-board/${encodeURIComponent(subjects)}`);
+    }, [code, subjects, history]);
+
+
+    return (
+        <div>
+            <h1>Google Auth Handler</h1>
+        </div>
+    );
 }
 
 export default GoogleAuthHandler;
