9902b29086a989d678f1de24c6f29f9bab7c43ed
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-14 21:51:46 +0700

    update the import statementt

--- frontend/src/views/auth/signIn/GoogleAuthRedirect.jsx
+++ frontend/src/views/auth/signIn/GoogleAuthRedirect.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useHistory, useLocation} from 'react-router-dom';
 
 const GoogleAuthHandler = () => {
