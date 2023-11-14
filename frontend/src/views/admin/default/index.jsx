1af6a0dd4ac40d202827fa3b6a949241b2f7f337
Nanthawat DUANG-EAD <nanthawat.boom@gmail.com>
2023-11-14 23:42:18 +0700

    Styling calendar light mode

--- frontend/src/views/admin/default/index.jsx
+++ frontend/src/views/admin/default/index.jsx
@@ -1,27 +1,30 @@
 // Chakra imports
 import {
-    Avatar,
-    Box,
-    Flex,
-    FormLabel,
-    Icon,
-    Select,
-    SimpleGrid,
-    useColorModeValue,
+  Avatar,
+  Box,
+  Flex,
+  FormLabel,
+  Icon,
+  Select,
+  SimpleGrid,
+  useColorModeValue,
 } from "@chakra-ui/react";
+
 // Assets
 import Usa from "assets/img/dashboards/usa.png";
+
 // Custom components
 import MiniCalendar from "components/calendar/MiniCalendar";
 import MiniStatistics from "components/card/MiniStatistics";
 import IconBox from "components/icons/IconBox";
 import React from "react";
 import {
-    MdAddTask,
-    MdAttachMoney,
-    MdBarChart,
-    MdFileCopy,
+  MdAddTask,
+  MdAttachMoney,
+  MdBarChart,
+  MdFileCopy,
 } from "react-icons/md";
+
 import CheckTable from "views/admin/default/components/CheckTable";
 import ComplexTable from "views/admin/default/components/ComplexTable";
 import DailyTraffic from "views/admin/default/components/DailyTraffic";
@@ -29,19 +32,20 @@ import PieCard from "views/admin/default/components/PieCard";
 import Tasks from "views/admin/default/components/Tasks";
 import TotalSpent from "views/admin/default/components/TotalSpent";
 import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-import {
-    columnsDataCheck,
-    columnsDataComplex,
-} from "views/admin/default/variables/columnsData";
-import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex
-    from "views/admin/default/variables/tableDataComplex.json";
 
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import {
+  columnsDataCheck,
+  columnsDataComplex,
+} from "views/admin/default/variables/columnsData";
+
+import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
+import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
+
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-
+// Set up momentLocalizer
 const localizer = momentLocalizer(moment);
 
 // Example data for events
@@ -59,22 +63,117 @@ const events = [
   // Add more events as needed
 ];
 
-// Your UserReports component
-export default function UserReports() {
-  // ... existing code
+// Custom event component
+const CustomEvent = ({ event }) => (
+  <div
+    style={{
+      backgroundColor: useColorModeValue('#6678b4', '#fdfdfd'),
+      color: useColorModeValue('#ffffff', '#091435'),
+      padding: '8px',
+      borderRadius: '5px',
+      boxShadow: '0 1px 2px rgba(0,0,0,0.1)',
+    }}
+  >
+    {event.title}
+  </div>
+);
 
+// Custom toolbar component
+const CustomToolbar = (toolbar) => (
+  <div
+    style={{
+      backgroundColor: useColorModeValue('#6678b4', '#fdfdfd'),
+      color: useColorModeValue('#ffffff', '#091435'),
+      padding: '10px',
+      borderRadius: '4px',
+      boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    }}
+  >
+    <div style={{ fontWeight: 'bold', fontSize: '18px' }}>
+      {toolbar.label}
+    </div>
+    <div>
+      {/* Apply hover effect to the buttons */}
+      <button
+        style={{
+          background: 'none',
+          borderRadius: '4px',
+          color: useColorModeValue('#ffffff', '#091435'),
+          cursor: 'pointer',
+          marginRight: '15px',
+          padding: '8px',
+          transition: 'background 0.3s ease',
+        }}
+        onClick={toolbar.onNavigate.bind(null, 'TODAY')}
+        // Add hover effect
+        onMouseOver={(e) => (e.target.style.background = '#33425d')}
+        onMouseOut={(e) => (e.target.style.background = 'none')}
+      >
+        Today
+      </button>
+      <button
+        style={{
+          background: 'none',
+          borderRadius: '4px',
+          color: useColorModeValue('#ffffff', '#091435'),
+          cursor: 'pointer',
+          marginRight: '15px',
+          padding: '8px',
+          transition: 'background 0.3s ease',
+        }}
+        onClick={toolbar.onNavigate.bind(null, 'PREV')}
+        // Add hover effect
+        onMouseOver={(e) => (e.target.style.background = '#33425d')}
+        onMouseOut={(e) => (e.target.style.background = 'none')}
+      >
+        Back
+      </button>
+      <button
+        style={{
+          background: 'none',
+          borderRadius: '4px',
+          color: useColorModeValue('#ffffff', '#091435'),
+          cursor: 'pointer',
+          padding: '8px',
+          transition: 'background 0.3s ease',
+        }}
+        onClick={toolbar.onNavigate.bind(null, 'NEXT')}
+        // Add hover effect
+        onMouseOver={(e) => (e.target.style.background = '#33425d')}
+        onMouseOut={(e) => (e.target.style.background = 'none')}
+      >
+        Next
+      </button>
+    </div>
+  </div>
+);
+
+const UserReports = () => {
   return (
     <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
       <Box mt="4">
-        <Calendar
+        <BigCalendar
           localizer={localizer}
           events={events}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 500 }}
+          style={{
+            height: 500,
+            borderRadius: '8px',
+            overflow: 'hidden',
+            boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
+          }}
+          components={{
+            event: CustomEvent,
+            toolbar: CustomToolbar,
+          }}
         />
       </Box>
     </Box>
   );
-}
+};
 
+export default UserReports;
