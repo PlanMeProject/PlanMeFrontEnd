8e8f4040f842ed9daa0548d54f1e0eae58f58e9a
Nanthawat DUANG-EAD <nanthawat.boom@gmail.com>
2023-11-14 21:03:59 +0700

    Add react big calendar to the default

--- frontend/src/views/admin/default/index.jsx
+++ frontend/src/views/admin/default/index.jsx
@@ -1,25 +1,3 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _|
- | |_| | | | | |_) || |  / / | | |  \| | | | | || |
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 // Chakra imports
 import {
     Avatar,
@@ -59,120 +37,44 @@ import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
 import tableDataComplex
     from "views/admin/default/variables/tableDataComplex.json";
 
-export default function UserReports() {
-    // Chakra Color Mode
-    const brandColor = useColorModeValue("brand.500", "white");
-    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
-    return (
-        <Box pt={{base: "130px", md: "80px", xl: "80px"}}>
-            <SimpleGrid
-                columns={{base: 1, md: 2, lg: 3, "2xl": 6}}
-                gap='20px'
-                mb='20px'>
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={boxBg}
-                            icon={
-                                <Icon w='32px' h='32px' as={MdBarChart}
-                                      color={brandColor}/>
-                            }
-                        />
-                    }
-                    name='Earnings'
-                    value='$350.4'
-                />
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={boxBg}
-                            icon={
-                                <Icon w='32px' h='32px' as={MdAttachMoney}
-                                      color={brandColor}/>
-                            }
-                        />
-                    }
-                    name='Spend this month'
-                    value='$642.39'
-                />
-                <MiniStatistics growth='+23%' name='Sales' value='$574.34'/>
-                <MiniStatistics
-                    endContent={
-                        <Flex me='-16px' mt='10px'>
-                            <FormLabel htmlFor='balance'>
-                                <Avatar src={Usa}/>
-                            </FormLabel>
-                            <Select
-                                id='balance'
-                                variant='mini'
-                                mt='5px'
-                                me='0px'
-                                defaultValue='usd'>
-                                <option value='usd'>USD</option>
-                                <option value='eur'>EUR</option>
-                                <option value='gba'>GBA</option>
-                            </Select>
-                        </Flex>
-                    }
-                    name='Your balance'
-                    value='$1,000'
-                />
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-                            icon={<Icon w='28px' h='28px' as={MdAddTask}
-                                        color='white'/>}
-                        />
-                    }
-                    name='New Tasks'
-                    value='154'
-                />
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={boxBg}
-                            icon={
-                                <Icon w='32px' h='32px' as={MdFileCopy}
-                                      color={brandColor}/>
-                            }
-                        />
-                    }
-                    name='Total Projects'
-                    value='2935'
-                />
-            </SimpleGrid>
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-            <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px' mb='20px'>
-                <TotalSpent/>
-                <WeeklyRevenue/>
-            </SimpleGrid>
-            <SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>
-                <CheckTable columnsData={columnsDataCheck}
-                            tableData={tableDataCheck}/>
-                <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>
-                    <DailyTraffic/>
-                    <PieCard/>
-                </SimpleGrid>
-            </SimpleGrid>
-            <SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>
-                <ComplexTable
-                    columnsData={columnsDataComplex}
-                    tableData={tableDataComplex}
-                />
-                <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>
-                    <Tasks/>
-                    <MiniCalendar h='100%' minW='100%' selectRange={false}/>
-                </SimpleGrid>
-            </SimpleGrid>
-        </Box>
-    );
+
+const localizer = momentLocalizer(moment);
+
+// Example data for events
+const events = [
+  {
+    title: 'Meeting 1',
+    start: new Date(2023, 10, 15, 10, 0),
+    end: new Date(2023, 10, 15, 11, 0),
+  },
+  {
+    title: 'Meeting 2',
+    start: new Date(2023, 10, 16, 14, 0),
+    end: new Date(2023, 10, 16, 15, 0),
+  },
+  // Add more events as needed
+];
+
+// Your UserReports component
+export default function UserReports() {
+  // ... existing code
+
+  return (
+    <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
+      <Box mt="4">
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </Box>
+    </Box>
+  );
 }
+
