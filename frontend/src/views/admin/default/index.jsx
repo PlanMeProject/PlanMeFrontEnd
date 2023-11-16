e277ae17af0a8fe61de2e35119e94bc3d659bed0
Nanthawat DUANG-EAD <nanthawat.boom@gmail.com>
2023-11-16 12:11:18 +0700

    Improve calendar styling on light/dark mode/ Add the weekly view

--- frontend/src/views/admin/default/index.jsx
+++ frontend/src/views/admin/default/index.jsx
@@ -1,160 +1,140 @@
-// Chakra imports
-import {
-  Avatar,
-  Box,
-  Flex,
-  FormLabel,
-  Icon,
-  Select,
-  SimpleGrid,
-  useColorModeValue,
-} from "@chakra-ui/react";
 
-// Assets
-import Usa from "assets/img/dashboards/usa.png";
 
-// Custom components
-import MiniCalendar from "components/calendar/MiniCalendar";
-import MiniStatistics from "components/card/MiniStatistics";
-import IconBox from "components/icons/IconBox";
-import React from "react";
-import {
-  MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
-  MdFileCopy,
-} from "react-icons/md";
-
-import CheckTable from "views/admin/default/components/CheckTable";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
-import Tasks from "views/admin/default/components/Tasks";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-
-import {
-  columnsDataCheck,
-  columnsDataComplex,
-} from "views/admin/default/variables/columnsData";
-
-import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
-
+import React, { useState} from "react";
+import { Box, Flex, Select , useColorModeValue} from "@chakra-ui/react";
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-// Set up momentLocalizer
 const localizer = momentLocalizer(moment);
 
-// Example data for events
 const events = [
-  {
-    title: 'Meeting 1',
-    start: new Date(2023, 10, 15, 10, 0),
-    end: new Date(2023, 10, 15, 11, 0),
-  },
-  {
-    title: 'Meeting 2',
-    start: new Date(2023, 10, 16, 14, 0),
-    end: new Date(2023, 10, 16, 15, 0),
-  },
-  // Add more events as needed
+  // Your event data here
 ];
 
-// Custom event component
 const CustomEvent = ({ event }) => (
   <div
     style={{
-      backgroundColor: useColorModeValue('#6678b4', '#fdfdfd'),
-      color: useColorModeValue('#ffffff', '#091435'),
+      backgroundColor: '#6678b4',
+      color: '#ffffff',
       padding: '8px',
       borderRadius: '5px',
       boxShadow: '0 1px 2px rgba(0,0,0,0.1)',
+      cursor: 'pointer',
+      transition: 'background 0.3s ease, color 0.3s ease',
     }}
   >
     {event.title}
   </div>
 );
 
-// Custom toolbar component
-const CustomToolbar = (toolbar) => (
-  <div
-    style={{
-      backgroundColor: useColorModeValue('#6678b4', '#fdfdfd'),
-      color: useColorModeValue('#ffffff', '#091435'),
-      padding: '10px',
-      borderRadius: '4px',
-      boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    }}
-  >
-    <div style={{ fontWeight: 'bold', fontSize: '18px' }}>
-      {toolbar.label}
+const CustomToolbar = (toolbar) => {
+  const buttonStyle = {
+    ...toolbarButtonStyle,
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: '#ffffff',
+        color: '#0b1437',
+        padding: '10px',
+        borderRadius: '4px',
+        boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ fontWeight: 'bold', fontSize: '18px' }}>
+        {toolbar.label}
+      </div>
+      <div>
+        <button
+          style={buttonStyle}
+          onClick={() => toolbar.onNavigate('TODAY')}
+        >
+          Today
+        </button>
+        <button
+          style={buttonStyle}
+          onClick={() => toolbar.onNavigate('PREV')}
+        >
+          Back
+        </button>
+        <button
+          style={buttonStyle}
+          onClick={() => toolbar.onNavigate('NEXT')}
+        >
+          Next
+        </button>
+      </div>
     </div>
-    <div>
-      {/* Apply hover effect to the buttons */}
-      <button
-        style={{
-          background: 'none',
-          borderRadius: '4px',
-          color: useColorModeValue('#ffffff', '#091435'),
-          cursor: 'pointer',
-          marginRight: '15px',
-          padding: '8px',
-          transition: 'background 0.3s ease',
-        }}
-        onClick={toolbar.onNavigate.bind(null, 'TODAY')}
-        // Add hover effect
-        onMouseOver={(e) => (e.target.style.background = '#33425d')}
-        onMouseOut={(e) => (e.target.style.background = 'none')}
-      >
-        Today
-      </button>
-      <button
-        style={{
-          background: 'none',
-          borderRadius: '4px',
-          color: useColorModeValue('#ffffff', '#091435'),
-          cursor: 'pointer',
-          marginRight: '15px',
-          padding: '8px',
-          transition: 'background 0.3s ease',
-        }}
-        onClick={toolbar.onNavigate.bind(null, 'PREV')}
-        // Add hover effect
-        onMouseOver={(e) => (e.target.style.background = '#33425d')}
-        onMouseOut={(e) => (e.target.style.background = 'none')}
-      >
-        Back
-      </button>
-      <button
-        style={{
-          background: 'none',
-          borderRadius: '4px',
-          color: useColorModeValue('#ffffff', '#091435'),
-          cursor: 'pointer',
-          padding: '8px',
-          transition: 'background 0.3s ease',
-        }}
-        onClick={toolbar.onNavigate.bind(null, 'NEXT')}
-        // Add hover effect
-        onMouseOver={(e) => (e.target.style.background = '#33425d')}
-        onMouseOut={(e) => (e.target.style.background = 'none')}
-      >
-        Next
-      </button>
-    </div>
-  </div>
-);
+  );
+};
+
+const toolbarButtonStyle = {
+  background: 'none',
+  borderRadius: '4px',
+  color: '#0b1437',
+  cursor: 'pointer',
+  marginRight: '15px',
+  padding: '8px',
+  transition: 'background 0.3s ease, color 0.3s ease',
+};
+
 
 const UserReports = () => {
+  const [view, setView] = useState('month');
+
+  const handleViewChange = (newView) => {
+    setView(newView);
+  };
+
+  // Custom function to get day properties
+  const dayPropGetter = (date) => {
+    const today = new Date();
+    const isToday = date.toDateString() === today.toDateString();
+    const isWeekend = date.getDay() === 0 || date.getDay() === 6;
+
+    // Example: Set different background colors for today and weekends
+    let backgroundColor = 'inherit';
+    if (isToday) {
+      backgroundColor = 'rgba(159,157,157,0.61)'; // Color for today
+    } else if (isWeekend) {
+      backgroundColor = 'rgba(159,157,157,0.28)'; // Color for weekends
+    }
+
+    return {
+      style: {
+        backgroundColor,
+      },
+    };
+  };
+
   return (
     <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
       <Box mt="4">
+        <Flex mb="4" justify="space-between" align="center">
+           <Select
+            value={view}
+            onChange={(e) => handleViewChange(e.target.value)}
+            width="150px"
+            marginRight="4"
+            // Customize the background color of the options here
+            color={useColorModeValue('#0b1437', '#ffffff')}
+            bg={useColorModeValue('#ffffff', '#1a202c')}
+            borderRadius="4px"
+            _focus={{ boxShadow: '0 0 0 1px #6678b4' }}
+          >
+            <option value="month" style={{ backgroundColor: '#1a202c', color: '#ffffff' }}>
+              Month
+            </option>
+            <option value="week" style={{ backgroundColor: '#1a202c', color: '#ffffff' }}>
+              Week
+            </option>
+          </Select>
+        </Flex>
         <BigCalendar
           localizer={localizer}
           events={events}
@@ -170,6 +150,12 @@ const UserReports = () => {
             event: CustomEvent,
             toolbar: CustomToolbar,
           }}
+          views={{
+            month: true,
+            week: true,
+          }}
+          view={view}
+          dayPropGetter={dayPropGetter} // Set the dayPropGetter function
         />
       </Box>
     </Box>
@@ -177,3 +163,5 @@ const UserReports = () => {
 };
 
 export default UserReports;
+
+
