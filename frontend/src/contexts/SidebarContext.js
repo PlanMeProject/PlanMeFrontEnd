4b259996beb87e29beafe125ca607d649e4adf55
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-17 21:33:16 +0700

    added sidebarProvider

--- frontend/src/contexts/SidebarContext.js
+++ frontend/src/contexts/SidebarContext.js
@@ -1,3 +1,18 @@
-import { createContext } from "react";
-
+import React, { createContext, useState } from 'react';
 export const SidebarContext = createContext();
+
+export const SidebarProvider = ({ children }) => {
+  const [userToken, setUserToken] = useState('');
+  const [userId, setUserId] = useState('');
+
+  const updateUser = (newToken, newUserId) => {
+    setUserToken(newToken);
+    setUserId(newUserId);
+  };
+
+  return (
+    <SidebarContext.Provider value={{ userToken, userId, updateUser }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
