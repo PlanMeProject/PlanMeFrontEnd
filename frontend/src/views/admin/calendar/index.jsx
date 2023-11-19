f946c8351b46ed3f367d4d229d3aaf3ec54af614
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-19 21:05:42 +0700

    added calendar

--- frontend/src/views/admin/calendar/index.jsx
+++ frontend/src/views/admin/calendar/index.jsx
@@ -1,178 +1,186 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _|
- | |_| | | | | |_) || |  / / | | |  \| | | | | || |
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
+import React, { useState, useEffect } from "react";
+import { Box, Flex, Select, useColorModeValue } from "@chakra-ui/react";
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
+const localizer = momentLocalizer(moment);
 
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
+const Calendar = () => {
+    const [tasks, setTasks] = useState([]);
+    const [view, setView] = useState('month');
 
-* Designed and Coded by Simmmple
+    useEffect(() => {
+        const userId = localStorage.getItem("userId");
+        const loadTasks = () => {
+            const selectedSubjectFromStorage = localStorage.getItem('selectedSubjects');
 
-=========================================================
+            if (selectedSubjectFromStorage === null) {
+                return;
+            }
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+            let selectedSubjects;
+            try {
+                selectedSubjects = JSON.parse(selectedSubjectFromStorage);
+            } catch (e) {
+                console.error("Error parsing JSON from localStorage:", e);
+                return;
+            }
 
-*/
+            const courseParams = selectedSubjects.map(course => `courses=${encodeURIComponent(course)}`).join('&');
+            const fetchURL = `http://127.0.0.1:8000/api/users/${userId}/tasks/?user_id=${userId}&${courseParams}`;
 
-// Chakra imports
-import {
-    Avatar,
-    Box,
-    Flex,
-    FormLabel,
-    Icon,
-    Select,
-    SimpleGrid,
-    useColorModeValue,
-} from "@chakra-ui/react";
-// Assets
-import Usa from "assets/img/dashboards/usa.png";
-// Custom components
-import MiniCalendar from "components/calendar/MiniCalendar";
-import MiniStatistics from "components/card/MiniStatistics";
-import IconBox from "components/icons/IconBox";
-import React from "react";
-import {
-    MdAddTask,
-    MdAttachMoney,
-    MdBarChart,
-    MdFileCopy,
-} from "react-icons/md";
-import CheckTable from "views/admin/calendar/components/CheckTable";
-import ComplexTable from "views/admin/calendar/components/ComplexTable";
-import DailyTraffic from "views/admin/calendar/components/DailyTraffic";
-import PieCard from "views/admin/calendar/components/PieCard";
-import Tasks from "views/admin/calendar/components/Tasks";
-import TotalSpent from "views/admin/calendar/components/TotalSpent";
-import WeeklyRevenue from "views/admin/calendar/components/WeeklyRevenue";
-import {
-    columnsDataCheck,
-    columnsDataComplex,
-} from "views/admin/calendar/variables/columnsData";
-import tableDataCheck from "views/admin/calendar/variables/tableDataCheck.json";
-import tableDataComplex
-    from "views/admin/calendar/variables/tableDataComplex.json";
+            fetch(fetchURL)
+                .then((response) => response.json())
+                .then((data) => {
+                    const taskData = data.data;
+                    if (taskData) {
+                        setTasks(taskData);
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error fetching data: ", error);
+                });
+        };
 
-export default function UserReports() {
-    // Chakra Color Mode
-    const brandColor = useColorModeValue("brand.500", "white");
-    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+        loadTasks();
+    }, []);
+
+    const events = tasks.map(task => {
+        const dueDate = new Date(task.attributes.due_date);
+        return {
+            title: task.attributes.title,
+            start: dueDate,
+            end: dueDate,
+            allDay: true
+        };
+    });
+
+    const CustomEvent = ({ event }) => (
+        <div
+            style={{
+                backgroundColor: useColorModeValue('#0b1437', '#ffffff'),
+                color: useColorModeValue('#ffffff', '#0b1437'),
+                padding: '3px',
+                borderRadius: '5px',
+                boxShadow: '0 1px 2px rgba(0,0,0,0.1)',
+                transition: 'background 0.3s ease, color 0.3s ease',
+            }}
+        >
+            <strong>{event.title}</strong>
+        </div>
+    );
+
+    const CustomToolbar = (toolbar) => (
+        <div
+            style={{
+                backgroundColor: '#ffffff',
+                color: '#0b1437',
+                padding: '10px',
+                borderRadius: '4px',
+                boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+            }}
+        >
+            <div style={{ fontWeight: 'bold', fontSize: '18px' }}>
+                {toolbar.label}
+            </div>
+            <div>
+                <button style={toolbarButtonStyle} onClick={() => toolbar.onNavigate('TODAY')}>
+                    Today
+                </button>
+                <button style={toolbarButtonStyle} onClick={() => toolbar.onNavigate('PREV')}>
+                    Back
+                </button>
+                <button style={toolbarButtonStyle} onClick={() => toolbar.onNavigate('NEXT')}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+
+    const toolbarButtonStyle = {
+        background: 'none',
+        borderRadius: '4px',
+        color: '#0b1437',
+        cursor: 'pointer',
+        marginRight: '15px',
+        padding: '8px',
+        transition: 'background 0.3s ease, color 0.3s ease',
+    };
+
+    const handleViewChange = (newView) => {
+        setView(newView);
+    };
+
+    const dayPropGetter = (date) => {
+        const today = new Date();
+        const isToday = date.toDateString() === today.toDateString();
+        const isWeekend = date.getDay() === 0 || date.getDay() === 6;
+
+        let backgroundColor = 'inherit';
+        if (isToday) {
+            backgroundColor = 'rgba(159,157,157,0.61)';
+        } else if (isWeekend) {
+            backgroundColor = 'rgba(159,157,157,0.28)';
+        }
+
+        return {
+            style: {
+                backgroundColor,
+            },
+        };
+    };
+
     return (
-        <Box pt={{base: "130px", md: "80px", xl: "80px"}}>
-            <SimpleGrid
-                columns={{base: 1, md: 2, lg: 3, "2xl": 6}}
-                gap='20px'
-                mb='20px'>
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={boxBg}
-                            icon={
-                                <Icon w='32px' h='32px' as={MdBarChart}
-                                      color={brandColor}/>
-                            }
-                        />
-                    }
-                    name='Earnings'
-                    value='$350.4'
-                />
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={boxBg}
-                            icon={
-                                <Icon w='32px' h='32px' as={MdAttachMoney}
-                                      color={brandColor}/>
-                            }
-                        />
-                    }
-                    name='Spend this month'
-                    value='$642.39'
-                />
-                <MiniStatistics growth='+23%' name='Sales' value='$574.34'/>
-                <MiniStatistics
-                    endContent={
-                        <Flex me='-16px' mt='10px'>
-                            <FormLabel htmlFor='balance'>
-                                <Avatar src={Usa}/>
-                            </FormLabel>
-                            <Select
-                                id='balance'
-                                variant='mini'
-                                mt='5px'
-                                me='0px'
-                                defaultValue='usd'>
-                                <option value='usd'>USD</option>
-                                <option value='eur'>EUR</option>
-                                <option value='gba'>GBA</option>
-                            </Select>
-                        </Flex>
-                    }
-                    name='Your balance'
-                    value='$1,000'
-                />
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-                            icon={<Icon w='28px' h='28px' as={MdAddTask}
-                                        color='white'/>}
-                        />
-                    }
-                    name='New Tasks'
-                    value='154'
-                />
-                <MiniStatistics
-                    startContent={
-                        <IconBox
-                            w='56px'
-                            h='56px'
-                            bg={boxBg}
-                            icon={
-                                <Icon w='32px' h='32px' as={MdFileCopy}
-                                      color={brandColor}/>
-                            }
-                        />
-                    }
-                    name='Total Projects'
-                    value='2935'
-                />
-            </SimpleGrid>
-
-            <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px' mb='20px'>
-                <TotalSpent/>
-                <WeeklyRevenue/>
-            </SimpleGrid>
-            <SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>
-                <CheckTable columnsData={columnsDataCheck}
-                            tableData={tableDataCheck}/>
-                <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>
-                    <DailyTraffic/>
-                    <PieCard/>
-                </SimpleGrid>
-            </SimpleGrid>
-            <SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>
-                <ComplexTable
-                    columnsData={columnsDataComplex}
-                    tableData={tableDataComplex}
-                />
-                <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>
-                    <Tasks/>
-                    <MiniCalendar h='100%' minW='100%' selectRange={false}/>
-                </SimpleGrid>
-            </SimpleGrid>
+        <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
+            <Flex mb="4" justify="space-between" align="center">
+                <Select
+                    value={view}
+                    onChange={(e) => handleViewChange(e.target.value)}
+                    width="150px"
+                    marginRight="4"
+                    color={useColorModeValue('#0b1437', '#ffffff')}
+                    bg={useColorModeValue('#ffffff', '#1a202c')}
+                    borderRadius="4px"
+                    _focus={{ boxShadow: '0 0 0 1px #6678b4' }}
+                >
+                    <option value="month" style={{ backgroundColor: useColorModeValue('#ffffff','#1a202c'), color: useColorModeValue('#1a202c','#ffffff') }}>
+                        Month
+                    </option>
+                    <option value="week" style={{ backgroundColor: useColorModeValue('#ffffff','#1a202c'), color: useColorModeValue('#1a202c','#ffffff') }}>
+                        Week
+                    </option>
+                </Select>
+            </Flex>
+            <BigCalendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                allDayMaxRows={3}
+                popup={true}
+                style={{
+                    height: 800,
+                    borderRadius: '8px',
+                    overflow: 'hidden',
+                    boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
+                }}
+                components={{
+                    event: CustomEvent,
+                    toolbar: CustomToolbar,
+                }}
+                views={{
+                    month: true,
+                    week: true,
+                }}
+                view={view}
+                dayPropGetter={dayPropGetter}
+            />
         </Box>
     );
-}
+};
+
+export default Calendar;
