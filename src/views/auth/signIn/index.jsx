5f95e0716e3f9c21579c325be71dc0871c8db50b
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-23 17:32:54 +0700

    check redirect login in index.jsx

--- src/views/auth/signIn/index.jsx
+++ src/views/auth/signIn/index.jsx
@@ -31,10 +31,10 @@ function SignIn() {
     useEffect(() => {
         console.log(storedToken);
         console.log(storedUserId);
-        // if (storedToken && storedUserId) {
-        //     // history.push(`/admin/task-board/${storedUserId}/${storedToken}`);
-        // }
-    }, []);
+        if (storedToken && storedUserId) {
+            history.push(`/admin/task-board/${storedUserId}/${storedToken}`);
+        }
+    }, [storedToken, storedUserId, history]);
 
     return (
         <DefaultAuth image={"frontend/src/assets/img/3dplanme.png"}>
