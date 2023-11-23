97f37de3721a4fc4eb9b736165b9769eae32c9d0
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-23 15:10:06 +0700

    open redirect url

--- src/views/auth/signIn/auth.js
+++ src/views/auth/signIn/auth.js
@@ -11,5 +11,5 @@ export const handleGoogleSignIn = () => {
     console.log('GOOGLE_CLIENT_ID', GOOGLE_CLIENT_ID);
     console.log('REDIRECT_URI', REDIRECT_URI);
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${encodeURIComponent(SCOPES.join(' '))}&access_type=offline&prompt=consent`;
-    // window.location.href = authUrl;
+    window.location.href = authUrl;
 };
