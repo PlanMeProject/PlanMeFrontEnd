ec4423556e31d467598fa694f436993581a52c00
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-11-26 19:40:06 +0700

    detect 404 when get assignment

--- src/views/admin/todoDashboard/index.jsx
+++ src/views/admin/todoDashboard/index.jsx
@@ -183,6 +183,9 @@ export default function UserReports() {
             setAssignments(data.data);
             setIsLoading(false);
         }).catch(error => {
+            if (error.status === 404) {
+                handleLogout();
+            }
             setMessage("You have selected a course that you are not a student.");
             setIsNoTasks(true);
             setIsLoading(false);
